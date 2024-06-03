7abde2af2aa09e66a9bf0cb22547ca35395b6225
magn20 <magnum.overgaar@hotmail.com>
2024-06-03 18:46:05 +0200

    ocelot

--- deployable projects/Gateway/Program.cs
+++ deployable projects/Gateway/Program.cs
@@ -1,8 +1,13 @@
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
