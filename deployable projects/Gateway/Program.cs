95d67ccc85dc35d076269c5e5c3257c0090d9787
magn20 <71693106+magn20@users.noreply.github.com>
2024-06-04 16:40:59 +0200

    Gateway with ocelot (#10)
    * ocelot
    
    * setup ocelot

--- deployable projects/Gateway/Program.cs
+++ deployable projects/Gateway/Program.cs
@@ -1,8 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,10 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseOcelot().Wait();
 
 app.MapControllers();
 
--- deployable projects/LocationAPI/Program.cs
+++ deployable projects/LocationAPI/Program.cs
@@ -45,7 +45,6 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
 app.MapControllers();
 
